953b3270bed1c51996b21d89f7e28f38a1f36cbf
Jeroen Metsemakers <j.metsemakers@live.nl>
2016-03-10 09:13:16 +0100

    Versie 2 - Basis: Beurten en HP

--- js/custom.js
+++ js/custom.js
@@ -1,42 +1,43 @@
-                                function StartGame() {
+$(document).ready(function () {
+    $("#startButton").click(function () {
 
-                                    document.getElementById("EnemyImage").innerHTML = '<img src=\'img/characters/Giovani.png\'>';
-
-                                    setTimeout(function () { document.getElementById("TextLine1").innerHTML = "<strong>Giovani</strong> wants to battle!!<br />"; }, 1000);
-
-                                    setTimeout(function () { document.getElementById("TextLine1").innerHTML = "<strong>Giovani</strong> sents out "; }, 2000);
-
-                                    var x = Math.floor((Math.random() * 6) + 1);
-                                    if (x == 1) {
-                                        setTimeout(function () { document.getElementById("PokemonName").innerHTML = "Charmander "; }, 2000);
-                                        setTimeout(function () { document.getElementById("PokemonImage").innerHTML = '<img src=\'img/pokemon/Charmander.png\'>'; }, 2500);
-                                    }
-                                    if (x == 2) {
-                                        setTimeout(function () { document.getElementById("PokemonName").innerHTML = "Squirtle "; }, 2000);
-                                        setTimeout(function () { document.getElementById("PokemonImage").innerHTML = '<img src=\'img/pokemon/Squirtle.png\'>'; }, 2500);
-                                    }
-                                    if (x == 3) {
-                                        setTimeout(function () { document.getElementById("PokemonName").innerHTML = "Ivysaur "; }, 2000);
-                                        setTimeout(function () { document.getElementById("PokemonImage").innerHTML = '<img src=\'img/pokemon/Ivysaur.png\'>'; }, 2500);
-                                    }
-                                    if (x == 4) {
-                                        setTimeout(function () { document.getElementById("PokemonName").innerHTML = "Haunter "; }, 2000);
-                                        setTimeout(function () {document.getElementById("PokemonImage").innerHTML = '<img src=\'img/pokemon/Haunter.png\'>'; }, 2500);
-                                    }
-                                    if (x == 5) {
-                                        setTimeout(function () { document.getElementById("PokemonName").innerHTML = "Alakazam "; }, 2000);
-                                        setTimeout(function () {document.getElementById("PokemonImage").innerHTML = '<img src=\'img/pokemon/Alakazam.png\'>'; }, 2500);
-                                    }
-                                    if (x == 6) {
-                                        setTimeout(function () { document.getElementById("PokemonName").innerHTML = "Machamp "; }, 2000);
-                                        setTimeout(function () {document.getElementById("PokemonImage").innerHTML = '<img src=\'img/pokemon/Machamp.png\'>'; }, 2500);
-                                    }
-
-                                    setTimeout(function () { document.getElementById("TextLine3").innerHTML = "!!"; }, 2000);
-
-                                    setTimeout(function () { document.getElementById("Textline4").innerHTML = "What would you like to do?"; }, 3000);
-
-                                    setTimeout(function () { document.getElementById("Textline5").innerHTML = "<button>fight</button> <button>run</button>"; }, 4000);
-
-                                    document.getElementById("StartButton").remove();
-                                }
+        var hp_player = 100;
+        var hp_console = 100;
+        var turn = true;
+        
+        var time_out = function (elem_id, delayms, input) {
+            setTimeout(function () {
+                $(elem_id).append(input);
+            }, delayms);
+        };        
+         
+        
+        while (hp_player >= 0 || hp_console >= 0) {
+            if (turn) {
+                hp_console = hp_console - 20;
+                time_out("#consoleInfo", 1000, hp_console);
+                if (hp_console < 1) {
+                    break;
+                }
+                console.log(hp_console);
+                turn = false;
+            } else {
+                hp_player = hp_player - 20;
+                time_out("#playerInfo", 1000, hp_console);                
+                if (hp_player < 1) {
+                    break;
+                }
+                console.log(hp_player);
+                turn = true;
+            }
+            ;
+        }
+        ;
+        if (hp_player <= 1) {
+            console.log("Console won! You lost!");
+            console.log("Console's score = " + hp_console);
+        } else if (hp_console <= 1) {
+            console.log("You won! Console lost!");
+        }
+    });
+});      
